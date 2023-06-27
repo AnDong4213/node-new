fb94e6063edf81e011090861aa29d23478db9ea0
赵 安东 <zhaoandong@bobfintech.com.cn>
2023-06-27 14:58:43 +0800

    feat: 入门

---
+++ entry-level/index.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+fs.open("./a.txt", "a+", function (err, fd) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(fd);
+  fs.writeFile(fd, "看看", function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+
+fs.mkdir("./tmp/test/", { recursive: true }, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("目录创建成功。");
+});
