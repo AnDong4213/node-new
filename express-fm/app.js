27d1351381d934facd9dd91e17f020b18457e5e5
赵 安东 <zhaoandong@bobfintech.com.cn>
2023-07-31 18:03:28 +0800

    fix: express

---
+++ express-fm/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+var fs = require("fs");
+var { promisify, callbackify } = require("util");
+const readFile = promisify(fs.readFile);
+
+/* app.get("/", function (req, res) {
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    if (!err) {
+      var back = JSON.parse(data);
+      res.send(back.users);
+    } else {
+      app.status(500).json({ err });
+    }
+  });
+}); */
+async function fn() {
+  return "hello world";
+}
+const callbackFunction = callbackify(fn);
+
+app.get("/", async function (req, res) {
+  try {
+    callbackFunction((err, ret) => {
+      if (err) throw err;
+      console.log(ret);
+    });
+    const data = await readFile("./db.json", "utf-8");
+    var back = JSON.parse(data);
+    res.send(back.users);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+app.post("/", async (req, res) => {
+  console.log("req.headers-", req.headers);
+  // console.log("req.body-", req.body);
+});
+
+app.listen(3000, () => {
+  console.log("Run http://127.0.0.1:3000");
+});
