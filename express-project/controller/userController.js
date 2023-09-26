55ff14ae165541a386beac0e147535284f5cc1e5
赵安东 <zhaoandong4213@sina.com>
2023-09-26 11:50:22 +0800

    fix:  图片上传

--- express-project/controller/userController.js
+++ express-project/controller/userController.js
@@ -1,5 +1,8 @@
+const fs = require("fs");
+const { promisify } = require("util");
 const { User } = require("../model");
 const { createToken } = require("../util/jwt");
+const rename = promisify(fs.rename);
 
 // 用户注册
 exports.register = async (req, res) => {
@@ -27,9 +30,25 @@ exports.login = async (req, res) => {
 // 用户修改
 exports.update = async (req, res) => {
   var id = req.user._id;
+  // mongoose自带的方法，{new: true}返回新的数据
   var dbBack = await User.findByIdAndUpdate(id, req.body, { new: true });
   // console.log(dbBack);
   res.status(200).json({ user: dbBack });
+};
+
+// 用户头像上传
+exports.headimg = async (req, res) => {
+  console.log("req.file--", req.file);
+  const { originalname, filename } = req.file;
+  const fileNames = originalname.split(".");
+  const fileType = fileNames[fileNames.length - 1];
+
+  try {
+    await rename(`./public/${filename}`, `./public/${filename}.${fileType}`);
+    res.status(200).json({ filepath: `${filename}.${fileType}` });
+  } catch (error) {
+    res.status(500).json({ err: error });
+  }
 };
 
 // 用户列表
--- express-project/router/user.js
+++ express-project/router/user.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const router = express.Router();
 const userController = require("../controller/userController");
 const validator = require("../middleware/validator/userValidator");
 const { verifyToken } = require("../util/jwt");
-const router = express.Router();
+
+const multer = require("multer");
+const upload = multer({ dest: "public/" });
 
 router
   .post("/register", validator.register, userController.register)
   .post("/login", validator.login, userController.login)
   .get("/list", verifyToken(), userController.list)
-  .put("/", verifyToken(), validator.update, userController.update);
+  .put("/", verifyToken(), validator.update, userController.update)
+  .post(
+    "/headimg",
+    verifyToken(),
+    upload.single("headimg"),
+    userController.headimg
+  );
 
 module.exports = router;
